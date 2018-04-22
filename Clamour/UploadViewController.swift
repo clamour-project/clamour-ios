0737c9bcd844a8c33bbc16cab767db5789253bac
San Nguyen <mr.sn5.kma@gmail.com>
2018-04-22 18:51:08 +0300

    -

--- Clamour/UploadViewController.swift
+++ Clamour/UploadViewController.swift
@@ -123,17 +123,24 @@ extension UploadViewController:  UIImagePickerControllerDelegate, UINavigationCo
     }
     
     func submit(image: UIImage) {
+        var resizedImage: UIImage!
+        if (image.size.width > 700) {
+            resizedImage = image.resizeWithWidth(width: 700)
+        } else {
+            resizedImage = image
+        }
         
         let session = URLSession(configuration: URLSessionConfiguration.default)
         
-        guard let url = URL(string: "https://clamour-server.appspot.com/clamour-api") else { return }
+        guard let url = URL(string: "https://server-clamour.appspot.com/clamour-api") else { return }
         //https://clamour-server.appspot.com/clamour-api
+        //https://clamourserverjava.appspot.com/clamour-api //polina
         //http://localhost:8080/loader
         var request = URLRequest(url: url)
         
         request.httpMethod = "POST"
         
-        let imageData: Data = UIImageJPEGRepresentation(image, 0.4)!
+        let imageData: Data = UIImageJPEGRepresentation(resizedImage, 0.4)!
         let imageStr = imageData.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
         request.httpBody = imageStr.data(using: String.Encoding.utf8)
         
@@ -155,3 +162,27 @@ extension UploadViewController:  UIImagePickerControllerDelegate, UINavigationCo
     
 }
 
+extension UIImage {
+    func resizeWithPercent(percentage: CGFloat) -> UIImage? {
+        let imageView = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: size.width * percentage, height: size.height * percentage)))
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = self
+        UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, false, scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        imageView.layer.render(in: context)
+        guard let result = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
+        UIGraphicsEndImageContext()
+        return result
+    }
+    func resizeWithWidth(width: CGFloat) -> UIImage? {
+        let imageView = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))))
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = self
+        UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, false, scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        imageView.layer.render(in: context)
+        guard let result = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
+        UIGraphicsEndImageContext()
+        return result
+    }
+}
