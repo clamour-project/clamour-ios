d3b6a5c3d100b9af1c23edd562ee1bd51f2f7458
San Nguyen <mr.sn5.kma@gmail.com>
2018-04-24 14:14:57 +0300

    match colours collection added

---
+++ Clamour/Views/Cells/ColorMatchViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ColorMatchViewCell.swift
+//  Clamour
+//
+//  Created by San Byn Nguyen on 24.04.2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ColorMatchViewCell: UICollectionViewCell {
+    @IBOutlet weak var colorRec: UIView!
+}
